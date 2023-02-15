ebf095d2883a52eb14dd02dcb229dc3b11e1e50d
Clayton Loftus <loftuscl@flip1.engr.oregonstate.edu>
2023-02-15 13:20:39 -0800

    Add data manipulation sql file

---
+++ data-manipulation-queries.sql
@@ -0,0 +1,119 @@
+------------------------------------------------------------------------------------
+-- SEEDS 
+------------------------------------------------------------------------------------
+
+-- Query for select all seeds
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new seed
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a seed
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a seed
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+------------------------------------------------------------------------------------
+-- CROPS 
+------------------------------------------------------------------------------------
+
+-- Query for select all crops
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new crop
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a crop
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a crop
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+------------------------------------------------------------------------------------
+-- SEASONS 
+------------------------------------------------------------------------------------
+
+-- Query for select all seasons
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+------------------------------------------------------------------------------------
+-- SALES
+------------------------------------------------------------------------------------
+
+-- Query for select all sales records
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new sale record
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a sale record
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a sale record
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+------------------------------------------------------------------------------------
+-- CUSTOMERS
+------------------------------------------------------------------------------------
+
+-- Query for select all customers
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new customer
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a customer
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a customer
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+------------------------------------------------------------------------------------
+-- CROPS_SEASONS
+------------------------------------------------------------------------------------
+
+-- Query for select all crops_seasons
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for add a new crop_season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for update a crop_season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
+
+-- Query for delete a crop_season
+-- Angle brackets '<' '>' used to denote the variables that will have data from Node
+-- TODO
