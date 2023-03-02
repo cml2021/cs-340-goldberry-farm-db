ca84e75c2954432a314b6fca9c8cff6893d49935
Clayton Loftus <loftuscl@flip1.engr.oregonstate.edu>
2023-03-02 10:59:07 -0800

    Add support for updating related crop on seed UPDATE

--- server.js
+++ server.js
@@ -177,19 +177,76 @@ app.patch("/update-seed", function(req, res) {
 	const growthDays = parseInt(data.growthDays);
 	const canRegrow = parseInt(data.canRegrow);
 	const relatedCropId = parseInt(data.relatedCropId);
-	
-	updateSeed = `UPDATE Seeds SET name = ?, price = ?, growth_days = ?, can_regrow = ? WHERE seed_id = ?;`
 
-	db.pool.query(updateSeed, [name, price, growthDays, canRegrow, seedId], function(error, rows, fields) {
+	getCurrentRelatedCropId = `SELECT Crops.crop_id FROM Crops WHERE Crops.seed_id = ?;`
+	updateSeed = `UPDATE Seeds SET name = ?, price = ?, growth_days = ?, can_regrow = ? WHERE seed_id = ?;`
+	removeRelatedCrop = `DELETE FROM Crops WHERE Crops.crop_id = ?;`
+	addRelatedCrop = `UPDATE Crops SET Crops.seed_id = ? WHERE Crops.crop_id = ?;`
+
+	// check if the seed being updated already has a related crop
+	db.pool.query(getCurrentRelatedCropId, [seedId], function(error, rows, fields) {
 		if (error) {
-			console.log(error);
-			res.sendStatus(400);
+			handleError(error);
 		} else {
-			res.body = JSON.stringify(data);
-			res.sendStatus(200);
-		}
+			const hasCurrentRelatedCrop = rows.length > 0 ? true : false;
+			const currentRelatedCropId = hasCurrentRelatedCrop === true ? rows[0].crop_id : -999;
+
+			// update the seed
+			db.pool.query(updateSeed, [name, price, growthDays, canRegrow, seedId], function(error, rows, fields) {
+				if (error) {
+					handleError(error);
+				
+				// if there is no change to the seed's related crop on UPDATE, return
+				} else if (currentRelatedCropId === relatedCropId) {
+					res.body = JSON.stringify(data);
+					res.sendStatus(200);
+				
+				// if a seed previously had a related crop and this was set to None on UPDATE, remove the relationship
+				// NOTE: this will also delete the crop since Crops.seed_id cannot be null
+				} else if (hasCurrentRelatedCrop === true && relatedCropId === -999) {
+					db.pool.query(removeRelatedCrop, [currentRelatedCropId], function(error, rows, fields) {
+						if (error) {
+							handleError(error);
+						} else {
+							res.body = JSON.stringify(data);
+							res.sendStatus(200);
+						}
+					})
+				
+				// if a seed had no related crop and this was set to some crop_id on UPDATE, add the relationship
+				} else if (hasCurrentRelatedCrop === false && relatedCropId != -999) {
+					db.pool.query(addRelatedCrop, [seedId, relatedCropId], function(error, rows, fields) {
+						if (error) {
+							handleError(error);
+						} else {
+							res.body = JSON.stringify(data);
+							res.sendStatus(200);
+						}
+					})
+
+				// if a seed previously had a related crop and this was set to a different crop_id on UPDATE, 
+				// remove the previous relationship and create a new relationship with the new related crop
+				// NOTE: this will also delete the previous related crop since Crops.seed_id cannot be null
+				} else {
+					db.pool.query(removeRelatedCrop, [currentRelatedCropId], function(error, rows, fields) {
+						if (error) {
+							handleError(error);
+						} else {
+							db.pool.query(addRelatedCrop, [seedId, relatedCropId], function(error, rows, fields) {
+								if (error) {
+									handleError(error);
+								} else {
+									res.body = JSON.stringify(data);
+									res.sendStatus(200);
+								}
+							})
+						}
+					})
+				}
+			}
+		)}
 	})
-})
+});
 
 app.delete("/delete-seed", function(req, res) {
 	const data = req.body
@@ -199,8 +256,7 @@ app.delete("/delete-seed", function(req, res) {
 
 	db.pool.query(deleteSeed, [seedId], function(error, rows, fields) {
 		if (error) {
-			console.log(error);
-			res.sendStatus(400);
+			handleError(error);
 		} else {
 			res.sendStatus(204);
 		}
@@ -225,14 +281,12 @@ app.get("/crops", function (req, res) {
 
 app.post("/add-crop", function(req, res) {
 	let data = req.body;
-	console.log(data);
 
 	let addCrop = `INSERT INTO Crops (name, quantity, unit_price, year, seed_id) VALUES ('${data['crop-name']}', '${data['crop-quantity']}', '${data['crop-unit-price']}', '${data['crop-year']}', '${data['related-seed']}');`;
 
 	db.pool.query(addCrop, function(error, rows, fields){
 		if (error) {
-			console.log(error);
-			res.sendStatus(400);
+			handleError(error);
 		}
 		else {
 			res.redirect('/crops');
@@ -257,14 +311,12 @@ app.post("/add-season", function(req, res) {
 
 	db.pool.query(addSeason, function(error, rows, fields) {
 		if (error) {
-			console.log(error);
-			res.sendStatus(400);
+			handleError(error);
 		} else {
 			res.redirect("/seasons");
 		}
 	})
 });
-
 
 app.get("/sales", function (req, res) {
 	res.render('sales');
@@ -277,6 +329,13 @@ app.get("/customers", function (req, res) {
 app.get("/crops-seasons", function (req, res) {
 	res.render('crops-seasons');
 });
+
+// error handler
+const handleError = (error) => {
+	console.log(error);
+	res.sendStatus(400);
+}
+
 
 // listener
 app.listen(PORT, function () {
